59738f9da667b0cb527383595ad179931dcb5964
Sridhar43 <panigrahisridhar464@gmail.com>
2026-07-24 19:16:04 +0530

    register rou te chsanged

--- email/src/services/api.js
+++ email/src/services/api.js
@@ -17,9 +17,9 @@ API.interceptors.request.use((config) => {
 
 // ==================== AUTH ====================
 
-export const registerUser = (data) => API.post("api/auth/register", data);
+export const registerUser = (data) => API.post("/api/auth/register", data);
 
-export const loginUser = (data) => API.post("/auth/login", data);
+export const loginUser = (data) => API.post("/api/auth/login", data);
 
 // ==================== DASHBOARD ====================
 
