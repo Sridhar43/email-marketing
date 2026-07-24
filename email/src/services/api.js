59105278eb32a3ce12d1d2c9931e9fc1a37ec16c
Sridhar43 <panigrahisridhar464@gmail.com>
2026-07-24 15:50:17 +0530

    configure baseurl in the basis of reander url

--- email/src/services/api.js
+++ email/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:4000/api",
+  baseURL: "https://email-marketing-mwp9.onrender.com",
 });
 
 // Automatically attach JWT token
